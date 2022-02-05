d87eb028a28ad664211ea668ba55b3adb5d52bc3
Voskrov <monicapeixoto0@gmail.com>
2022-02-05 15:41:15 -0300

    mudança de div

--- src/index.js
+++ src/index.js
@@ -1,36 +1,43 @@
 import validator from './validator.js'
 
-
-// const numberNo = document.getElementById("noNumber")
 const button1 = document.getElementById("button1")
+const button2 = document.getElementById("button2")
 const receberNum = document.getElementById("cardnumber")
 const span = document.getElementById("span")
-const result = document.getElementById("result")
-// function press(event){
-//   console.log("oi")
-//   var x = event.charCode
-//   if (x >= 48 && x <= 57) {
-//     return true
-//   }else{
-//     return false
-//   }
-// }
-
-// receberNum.addEventListener("keydown", press)
-
 let cardNum
+const messageRed = "<br><p>não é válido. Digite novamente.</p>"
+const messageGreen = "<br><p>é válido. Continue para as próximas etapas.</p>"
+const message = "<p>O número de cartão com final:<br></p>"
+
+function change(){
+  if (document.getElementById("resultt").style.display = 'none'){
+    document.getElementById("resultt").style.display = 'block'
+    document.getElementById("validator").style.display = 'none'
+    document.getElementById("information").style.display = 'none'
+  }
+}
+function changerev(){
+  if (document.getElementById("resultt").style.display = 'block'){
+    document.getElementById("result").textContent = ""
+    document.getElementById("resultt").style.display = 'none'
+    document.getElementById("validator").style.display = 'block'
+    document.getElementById("information").style.display = 'block'  
+  }
+}
+
 function getcardnumber() {
   if (receberNum.value.length >= 13 && receberNum.value.length <= 16 && Number(receberNum.value)) {
+    change()
     document.getElementById("span").textContent = ""
     cardNum = (receberNum.value)
     let cardmask = validator.maskify(cardNum)
     let validatorr = validator.isValid(cardNum)
     if (validatorr) {
       document.getElementById("cardnumber").value = ""
-      result.textContent = "O número de cartão com final: " + cardmask + " é válido. Continue para as próximas etapas."
+      result.insertAdjacentHTML("afterbegin", message + cardmask + messageGreen) 
     } else {
       document.getElementById("cardnumber").value = ""
-      result.textContent = "O número de cartão com final: " + cardmask + " não é válido. Digite novamente."
+      result.insertAdjacentHTML('afterbegin' , message + cardmask + messageRed)
     }
   }else{
     span.textContent = "Digite apenas números. Um cartão deve ter entre 13 a 16 números."
@@ -38,7 +45,4 @@ function getcardnumber() {
 }
 
 button1.addEventListener("click", getcardnumber)
-
-
-
-// #1c0021 #240047 #1c3166  #00a9d4 #40ffdc
+button2.addEventListener("click", changerev)
