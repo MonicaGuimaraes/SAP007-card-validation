06ee61aaca986b6f668412b62abbc648f6520237
Voskrov <monicapeixoto0@gmail.com>
2022-02-06 17:39:23 -0300

    adicionei footer e arrumei condição de sumir o validator

--- src/index.js
+++ src/index.js
@@ -4,20 +4,23 @@ const button1 = document.getElementById("button1")
 const button2 = document.getElementById("button2")
 const receberNum = document.getElementById("cardnumber")
 const span = document.getElementById("span")
+const result = document.getElementById("result")
 let cardNum
 const messageRed = "<br><p>não é válido. Digite novamente.</p>"
 const messageGreen = "<br><p>é válido. Continue para as próximas etapas.</p>"
 const message = "<p>O número de cartão com final:<br></p>"
 
 function change(){
-  if (document.getElementById("resultt").style.display = 'none'){
+  document.getElementById("resultt").style.display = 'none'
+  if (document.getElementById("resultt").style.display == 'none'){
     document.getElementById("resultt").style.display = 'block'
     document.getElementById("validator").style.display = 'none'
     document.getElementById("information").style.display = 'none'
   }
 }
 function changerev(){
-  if (document.getElementById("resultt").style.display = 'block'){
+  document.getElementById("resultt").style.display = 'block'
+  if (document.getElementById("resultt").style.display == 'block'){
     document.getElementById("result").textContent = ""
     document.getElementById("resultt").style.display = 'none'
     document.getElementById("validator").style.display = 'block'
