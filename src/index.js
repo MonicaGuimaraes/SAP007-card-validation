be0f04c10ee1befc42c929057918d64b33d2ab79
Voskrov <monicapeixoto0@gmail.com>
2022-02-07 16:00:11 -0300

    passei a function para o index

--- src/index.js
+++ src/index.js
@@ -28,6 +28,75 @@ function changerev(){
   }
 }
 
+function flag(numeroCart){
+  let bandeira = ''
+  if(numeroCart.charAt(0)==4){
+    bandeira = ' Visa'
+    return bandeira
+  }
+  if(numeroCart.charAt(0)==3){
+    if(numeroCart.charAt(0)==3 && numeroCart.charAt(1)==4){
+      bandeira = " American Express"
+      return bandeira
+    }else if(numeroCart.charAt(1)==6){
+      bandeira = " Diner's Club / International"
+      return bandeira
+    }
+    else if(numeroCart.charAt(1)==7){
+      bandeira = " American Express"
+      return bandeira
+    }
+    else if(numeroCart.charAt(1)==8){
+      bandeira = " Diner's Club / Carte Blanche"
+      return bandeira
+    }else if(numeroCart.charAt(1)==0 && numeroCart.charAt(2)>=0 && 
+    numeroCart.charAt(2)<=5){
+      bandeira = " Diner's Club / Carte Blanche"
+      return bandeira
+    }else{
+      bandeira = " JBC"
+      return bandeira
+    }
+  }  
+  if(numeroCart.charAt(0)==5 && numeroCart.charAt(1)>=1 && 
+  numeroCart.charAt(1)<=5){
+    bandeira = " MasterCard"
+    return bandeira
+  }
+  if(numeroCart.charAt(0)==6 && numeroCart.charAt(1)==0 && 
+  numeroCart.charAt(2)==1 && numeroCart.charAt(3)==1){
+    bandeira = " Discover"
+    return bandeira
+  }
+  if( numeroCart.charAt(0)==1 && numeroCart.charAt(1)==8 &&
+   numeroCart.charAt(2)==0 && numeroCart.charAt(3)==0){
+    bandeira = " JCB"
+    return bandeira
+  }
+  if( numeroCart.charAt(0)==2){
+    if(numeroCart.charAt(1)==1){
+      if(numeroCart.charAt(2)==3 && numeroCart.charAt(3)==1){
+        bandeira = ' JCB'
+        return bandeira
+      }else if(numeroCart.charAt(2)==4 && numeroCart.charAt(3)==9){
+        bandeira = " Diner's Club / enRoute"
+        return bandeira
+      }
+    }else if(numeroCart.charAt(1)==0 && numeroCart.charAt(2)==1 && 
+    numeroCart.charAt(3)==4){
+      bandeira = " Diner's Club / enRoute"
+      return bandeira
+    }
+  }
+  if(numeroCart.charAt(0)==6 && numeroCart.charAt(1)==0 && numeroCart.charAt(2)==6 &&
+  numeroCart.charAt(3)==2){
+    bandeira = " HiperCard"
+    return bandeira
+  }
+  bandeira = " sem bandeira"
+  return bandeira
+}
+
 function getcardnumber() {
   if (receberNum.value.length >= 13 && receberNum.value.length <= 16 && Number(receberNum.value)) {
     change()
@@ -35,7 +104,7 @@ function getcardnumber() {
     cardNum = (receberNum.value)
     let cardmask = validator.maskify(cardNum)
     let validatorr = validator.isValid(cardNum)
-    let bandeiraa = validator.flag(cardNum)
+    let bandeiraa = flag(cardNum)
     if (validatorr) {
       document.getElementById("cardnumber").value = ""
       result.insertAdjacentHTML("afterbegin", message + cardmask + bandeiraa + messageGreen) 
