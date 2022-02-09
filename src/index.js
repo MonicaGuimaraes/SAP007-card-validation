b4cf3485f6b38117b59848390031275e6e7b175f
Voskrov <monicapeixoto0@gmail.com>
2022-02-09 11:35:57 -0300

    Finalizado por enquanto

--- src/index.js
+++ src/index.js
@@ -11,86 +11,86 @@ const messageRed = "<br><p>não é válido. Digite novamente.</p>"
 const messageGreen = "<br><p>é válido. Continue para as próximas etapas.</p>"
 const message = "<p>O número de cartão com final:<br></p>"
 
-function changePageResult(){
+function changePageResult() {
   document.getElementById("result").style.display = 'none'
-  if (document.getElementById("result").style.display == 'none'){
+  if (document.getElementById("result").style.display == 'none') {
     document.getElementById("result").style.display = 'block'
     document.getElementById("validator").style.display = 'none'
     document.getElementById("information").style.display = 'none'
   }
 }
-function changePageToInitial(){
+function changePageToInitial() {
   document.getElementById("result").style.display = 'block'
-  if (document.getElementById("result").style.display == 'block'){
+  if (document.getElementById("result").style.display == 'block') {
     document.getElementById("result-article").textContent = ""
     document.getElementById("result").style.display = 'none'
     document.getElementById("validator").style.display = 'block'
-    document.getElementById("information").style.display = 'block'  
+    document.getElementById("information").style.display = 'block'
   }
 }
 
-function flag(numbersCard){
+function flag(numbersCard) {
   let flagCard = ''
-  if(numbersCard.charAt(0)==4){
+  if (numbersCard.charAt(0) == 4) {
     flagCard = ' Visa'
     return flagCard
   }
-  if(numbersCard.charAt(0)==3){
-    if(numbersCard.charAt(0)==3 && numbersCard.charAt(1)==4){
+  if (numbersCard.charAt(0) == 3) {
+    if (numbersCard.charAt(0) == 3 && numbersCard.charAt(1) == 4) {
       flagCard = " American Express"
       return flagCard
-    }else if(numbersCard.charAt(1)==6){
+    } else if (numbersCard.charAt(1) == 6) {
       flagCard = " Diner's Club / International"
       return flagCard
     }
-    else if(numbersCard.charAt(1)==7){
+    else if (numbersCard.charAt(1) == 7) {
       flagCard = " American Express"
       return flagCard
     }
-    else if(numbersCard.charAt(1)==8){
+    else if (numbersCard.charAt(1) == 8) {
       flagCard = " Diner's Club / Carte Blanche"
       return flagCard
-    }else if(numbersCard.charAt(1)==0 && numbersCard.charAt(2)>=0 && 
-    numbersCard.charAt(2)<=5){
+    } else if (numbersCard.charAt(1) == 0 && numbersCard.charAt(2) >= 0 &&
+      numbersCard.charAt(2) <= 5) {
       flagCard = " Diner's Club / Carte Blanche"
       return flagCard
-    }else{
+    } else {
       flagCard = " JBC"
       return flagCard
     }
-  }  
-  if(numbersCard.charAt(0)==5 && numbersCard.charAt(1)>=1 && 
-  numbersCard.charAt(1)<=5){
+  }
+  if (numbersCard.charAt(0) == 5 && numbersCard.charAt(1) >= 1 &&
+    numbersCard.charAt(1) <= 5) {
     flagCard = " MasterCard"
     return flagCard
   }
-  if(numbersCard.charAt(0)==6 && numbersCard.charAt(1)==0 && 
-  numbersCard.charAt(2)==1 && numbersCard.charAt(3)==1){
+  if (numbersCard.charAt(0) == 6 && numbersCard.charAt(1) == 0 &&
+    numbersCard.charAt(2) == 1 && numbersCard.charAt(3) == 1) {
     flagCard = " Discover"
     return flagCard
   }
-  if( numbersCard.charAt(0)==1 && numbersCard.charAt(1)==8 &&
-   numbersCard.charAt(2)==0 && numbersCard.charAt(3)==0){
+  if (numbersCard.charAt(0) == 1 && numbersCard.charAt(1) == 8 &&
+    numbersCard.charAt(2) == 0 && numbersCard.charAt(3) == 0) {
     flagCard = " JCB"
     return flagCard
   }
-  if( numbersCard.charAt(0)==2){
-    if(numbersCard.charAt(1)==1){
-      if(numbersCard.charAt(2)==3 && numbersCard.charAt(3)==1){
+  if (numbersCard.charAt(0) == 2) {
+    if (numbersCard.charAt(1) == 1) {
+      if (numbersCard.charAt(2) == 3 && numbersCard.charAt(3) == 1) {
         flagCard = ' JCB'
         return flagCard
-      }else if(numbersCard.charAt(2)==4 && numbersCard.charAt(3)==9){
+      } else if (numbersCard.charAt(2) == 4 && numbersCard.charAt(3) == 9) {
         flagCard = " Diner's Club / enRoute"
         return flagCard
       }
-    }else if(numbersCard.charAt(1)==0 && numbersCard.charAt(2)==1 && 
-    numbersCard.charAt(3)==4){
+    } else if (numbersCard.charAt(1) == 0 && numbersCard.charAt(2) == 1 &&
+      numbersCard.charAt(3) == 4) {
       flagCard = " Diner's Club / enRoute"
       return flagCard
     }
   }
-  if(numbersCard.charAt(0)==6 && numbersCard.charAt(1)==0 && numbersCard.charAt(2)==6 &&
-  numbersCard.charAt(3)==2){
+  if (numbersCard.charAt(0) == 6 && numbersCard.charAt(1) == 0 && numbersCard.charAt(2) == 6 &&
+    numbersCard.charAt(3) == 2) {
     flagCard = " HiperCard"
     return flagCard
   }
@@ -107,12 +107,12 @@ function getCardNumber() {
     let validatorBoolean = validator.isValid(cardNum)
     let flagStr = flag(cardNum)
     if (validatorBoolean) {
-      result.insertAdjacentHTML("afterbegin", message + cardMask + flagStr + messageGreen) 
+      result.insertAdjacentHTML("afterbegin", message + cardMask + flagStr + messageGreen)
     } else {
-      result.insertAdjacentHTML('afterbegin' , message + cardMask + messageRed)
+      result.insertAdjacentHTML('afterbegin', message + cardMask + messageRed)
     }
     receiveNum.value = ""
-  }else{
+  } else {
     span.textContent = "Digite apenas números. Um cartão deve ter entre 13 a 16 números."
   }
 }
--- src/validator.js
+++ src/validator.js
@@ -1,38 +1,38 @@
-const validator = { 
-   maskify: function(numberCard){
-     let mask = ""
-     const fourLastNumbers = 4
-     for (let ind=0; ind < numberCard.length; ind++){
-      if (ind >= (numberCard.length - fourLastNumbers)){
+const validator = {
+  maskify: function (numberCard) {
+    let mask = ""
+    const fourLastNumbers = 4
+    for (let ind = 0; ind < numberCard.length; ind++) {
+      if (ind >= (numberCard.length - fourLastNumbers)) {
         mask += numberCard.charAt(ind)
-      }else{
+      } else {
         mask += "#"
       }
-     }
-     return mask
-   },
-   
-  isValid: function(numberCard){
+    }
+    return mask
+  },
+
+  isValid: function (numberCard) {
     const cardList = Array.from(numberCard)
     let cardListRev = cardList.reverse()
     let sum = 0
 
-    for (let i=0; i < cardListRev.length; i++){
+    for (let i = 0; i < cardListRev.length; i++) {
       let digit = Number(cardListRev[i])
 
-      if (i % 2 !== 0){
+      if (i % 2 !== 0) {
         digit = digit * 2
-        if(digit > 9){
+        if (digit > 9) {
           digit -= 9
         }
-      } 
-        sum += digit
+      }
+      sum += digit
     }
 
-    if (sum % 10 == 0){
+    if (sum % 10 == 0) {
       return true
 
-    }else{
+    } else {
       return false
     }
   }
